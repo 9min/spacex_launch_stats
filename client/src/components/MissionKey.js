48861fc25a23124f0c302e55474d76076408b761
adam.html <adam.html@kakaomobility.com>
2018-12-24 14:36:58 +0900

    Finishing The App

---
+++ client/src/components/MissionKey.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function MissionKey() {
+  // const style = {}
+  return (
+    <div className="my-3" style={{ color: '#ffffff' }}>
+      <p>
+        <span className="px-3 mr-2 bg-success">Success</span>
+        <span className="px-3 mr-2 bg-danger">Fail</span>
+      </p>
+    </div>
+  )
+};
